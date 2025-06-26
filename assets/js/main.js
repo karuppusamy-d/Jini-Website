eb84b9db2acb83aa78f1f09a00141b990a5016f2
Karuppusamy <d.karuppusamy@outlook.com>
2025-06-27 01:10:45 +0530

    Add testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,6 +178,42 @@ if (select(".testimonials-slider")) {
   });
 }
 
+// Testimonials carousel (Products page)
+if (select(".testimonials-swiper")) {
+  new Swiper(".testimonials-swiper", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        centeredSlides: true,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: true,
+      },
+    },
+  });
+}
+
 // Mobile slider
 
 if (select(".mobile-slider")) {
